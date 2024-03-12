fb7e50310a7735bdc34aa06fb819cfffe66bfb3b
Denys Kolomiiets <106629418+DenysKo88@users.noreply.github.com>
2024-03-12 11:28:58 +0200

    + initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import axios from "axios";
 function App() {
+  // const cityCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=kyiv&appid=619c485f9412948c1be0d4bb8f4bea4c`
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.4333&lon=30.5167&appid=619c485f9412948c1be0d4bb8f4bea4c`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="top">
+        <div className="location">
+          <p>Kyiv</p>
+        </div>
+        <div className="temp">
+          <h1>2°C</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>-1°C</p>
+        </div>
+        <div className="humidity">
+          <p>80%</p>
+        </div>
+        <div className="wind">
+          <p>15km/h</p>
+        </div>
+      </div>
     </div>
   );
 }
